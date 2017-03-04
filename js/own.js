dbe01c4b6979104422628df748c055d585d76b49
sevczik11@gmail.com <sevczik11@gmail.com>
2017-03-04 22:58:02 +0200

    separate js file

---
+++ js/own.js
@@ -0,0 +1,8 @@
+function welcomeFunction(x) {
+			x.style.marginLeft = "15.2%";
+			x.style.transition = "all 0.3s ease-out 0s";
+		}
+
+		function outFunction(y) {
+			y.style.marginLeft = "15%";
+		}
