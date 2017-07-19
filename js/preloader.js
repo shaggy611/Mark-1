656a723c073989a41bf596195e8ca783caaa37b6
pathfinder16 <romanyuk.vsevolod@gmail.com>
2017-07-19 15:52:55 +0300

    prel

--- js/preloader.js
+++ js/preloader.js
@@ -1,8 +1,9 @@
 document.body.onload = function () {
 	setTimeout(function () {
 		var preloader = document.getElementById('prel');
-		if(!preloader.classList.contains('done')) {
+		/*if(!preloader.classList.contains('done')) {
 			preloader.classList.add('done');
-		}
+		}*/
+		preloader.classList.add('done');
 	}, 1000);
 };
